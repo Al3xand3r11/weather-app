20217b309c91a2f27bcb6102a8be88960f76207a
Al3xand3r11 <alexander.nance11@gmail.com>
2022-10-13 11:31:03 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, {useState}  from 'react'
+import axios from 'axios'
+
 
 function App() {
+
+ // const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=750cf243eb3ef9f09917f61ed547ee9d`
+ 
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='container'>
+        <div className='top'>
+          <div className='Location'>
+            <p>City</p>
+          </div>
+          <div className='temp'>
+            <h1>TestTemp</h1>
+          </div>
+          <div className='Description'>
+            <h2>Description</h2>
+          </div>
+        </div>
+        <div className='bottom'>
+          <div className='Feels'>
+            <p className ='bold'>Example Degrees</p>
+            <p>Feels like</p>
+          </div>
+          <div className='Humidity'>
+            <p className='bold'>Example Humidity</p>
+            <p>Test Humidity</p>
+          </div>
+          <div className = 'Wind'>
+            <p className='bold'>Example Wind</p>
+            <p>Windy</p>
+          </div>
+
+
+        </div>
+      </div>
+      
+        
+        
+        
+    
+      
     </div>
   );
 }
