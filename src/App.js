2fd9bbe8ccb7ae428a55c349537bca162d982961
Al3xand3r11 <alexander.nance11@gmail.com>
2022-10-13 11:50:08 -0700

    added search

--- src/App.js
+++ src/App.js
@@ -4,14 +4,32 @@ import axios from 'axios'
 
 
 function App() {
+  const [data,setData] = useState({})
+  const [location,setLocation] = useState('')
 
- // const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=750cf243eb3ef9f09917f61ed547ee9d`
+ const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=750cf243eb3ef9f09917f61ed547ee9d`
  
-
+const searchLocation = (event) => {
+  if (event.key === 'Enter') {
+  axios.get(url).then((response) => {
+    setData(response.data)
+    console.log(response.data)
+  })
+  }
+}
 
 
   return (
     <div className="app">
+      <div className='Search'>
+        <input
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Enter Location'
+        type="text"/>
+        
+      </div>
       <div className='container'>
         <div className='top'>
           <div className='Location'>
